3c8ba5169c489e55c8c420a62bb38ae1fe3571c9
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-11 00:26:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,9 +10,8 @@ def using_pop(array)
   array.pop 
 end  
 
-def pop_with_args(array,two)
-  array.pop(two)
-  array.pop(two)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array)
